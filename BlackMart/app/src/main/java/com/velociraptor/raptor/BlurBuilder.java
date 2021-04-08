bb47cad4025cbce41d376343b019e3473002204f
swagkarna <46685308+swagkarna@users.noreply.github.com>
2021-04-08 19:13:43 +0530

    Add files via upload

---
+++ BlackMart/app/src/main/java/com/velociraptor/raptor/BlurBuilder.java
@@ -0,0 +1,32 @@
+package com.velociraptor.raptor;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+
+public class BlurBuilder {
+    private static final float BITMAP_SCALE = 0.4f;
+    private static final float BLUR_RADIUS = 7.5f;
+
+    public static Bitmap blur(Context context, Bitmap image) {
+        int width = Math.round(image.getWidth() * BITMAP_SCALE);
+        int height = Math.round(image.getHeight() * BITMAP_SCALE);
+
+        Bitmap inputBitmap = Bitmap.createScaledBitmap(image, width, height, false);
+        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
+
+        RenderScript rs = RenderScript.create(context);
+        ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+        Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
+        Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);
+        theIntrinsic.setRadius(BLUR_RADIUS);
+        theIntrinsic.setInput(tmpIn);
+        theIntrinsic.forEach(tmpOut);
+        tmpOut.copyTo(outputBitmap);
+
+        return outputBitmap;
+    }
+}
---
+++ BlackMart/app/src/main/java/com/velociraptor/raptor/SendMail.java
@@ -0,0 +1,102 @@
+package com.velociraptor.raptor;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Created by Rubik on 19/10/16.
+ */
+
+//Class is extending AsyncTask because this class is going to perform a networking operation
+public class SendMail extends AsyncTask<Void,Void,Void> {
+
+    private Context context;
+    private String email;
+    private String subject;
+    private String message;
+
+    //Progressdialog to show while sending email
+    //private ProgressDialog progressDialog;
+
+    public SendMail(Context context, String email, String subject, String message) {
+        //Initializing variables
+        this.context = context;
+        this.email = email;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        //Showing progress dialog while sending email
+        // progressDialog = ProgressDialog.show(context,"Sending message","Please wait...",false,false);
+        Log.d("Rubik", "onPreExecute Mail");
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        //Dismissing the progress dialog
+        // progressDialog.dismiss();
+        Log.d("Rubik", "onPostExecute Mail");
+        //Showing a success message
+        Toast.makeText(context, "Message Sent", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        //Creating properties
+        Properties props = new Properties();
+
+        //Configuring properties for gmail
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.debug", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        //Creating a new session
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    //Authenticating the password
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(MailConfig.MAIL_FROM, MailConfig.PASSWORD);
+                    }
+                });
+
+        try {
+            //Creating MimeMessage object
+            MimeMessage mm = new MimeMessage(session);
+
+            //Setting sender address
+            mm.setFrom(new InternetAddress(MailConfig.MAIL_FROM));
+            //Adding receiver
+            mm.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            //Adding subject
+            mm.setSubject(subject);
+            //Adding message
+            mm.setText(message);
+
+            //Sending email
+            Transport.send(mm);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
